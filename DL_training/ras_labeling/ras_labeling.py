1c4c6e2478c037cb97ddea7375fcbdc870caf574
Ajinkya Khoche <khoche@kth.se>
2018-10-28 16:35:22 +0100

    trained network, sanitized the repo

---
+++ DL_training/ras_labeling/ras_labeling.py
@@ -0,0 +1,22 @@
+import sys
+import argparse
+
+import cv2
+print(cv2.__version__)
+
+def extractImages():
+    count = 0
+    vidcap = cv2.VideoCapture('vid2.mp4')
+    success,image = vidcap.read()
+    success = True
+    while success:
+      vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
+      success,image = vidcap.read()
+      print ('Read a new frame: ', success)
+      cv2.imwrite( "./TEMP_Folder/" + str(count+1000) + ".jpg", image)     # save frame as JPEG file
+      count = count + 1
+
+if __name__=="__main__":
+    #print("aba")
+
+    extractImages()
