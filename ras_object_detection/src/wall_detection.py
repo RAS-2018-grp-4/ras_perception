3db35e46e57bac1320cff4dbea1d0bd4d4639bde
Landzs <landzs@hotmail.com>
2018-11-18 14:30:07 +0100

    Update Wall Detection

--- ras_object_detection/src/wall_detection.py
+++ ras_object_detection/src/wall_detection.py
@@ -28,17 +28,16 @@ class Wall_Detection:
         self.ROBOT_POSITION = nav_msgs.msg.Odometry()
         self.MSG = std_msgs.msg.Bool()
         self.MSG.data = False
-        
+        self.PUB_POS = False
     #####################################################
     #             Initialize ROS Parameter              #
     #####################################################
         rospy.init_node('wall_detection_node', anonymous=True)
         self.pub_WALL_DETECTION = rospy.Publisher('/wall_detection', std_msgs.msg.Bool, queue_size=1)
         self.pub_WALL_POSITION = rospy.Publisher('/wall_position', geometry_msgs.msg.PoseArray, queue_size=1)
-        self.pub_WALL_DISTANCE = rospy.Publisher('/wall_distance', std_msgs.msg.Float32MultiArray, queue_size=1)
         self.rate = rospy.Rate(10)
         rospy.Subscriber('/scan', sensor_msgs.msg.LaserScan, self.feedback_laser)
-        rospy.Subscriber('/robot_odom', nav_msgs.msg.Odometry, self.feedback_odom)
+        rospy.Subscriber('/robot_filter', nav_msgs.msg.Odometry, self.feedback_odom)
 
         self.LISTENER = tf.TransformListener()
         
@@ -47,46 +46,25 @@ class Wall_Detection:
     #                   Laser_Feedback                  #
     #####################################################
     def feedback_laser(self,scan):
-        '''
-        # initalize distance sensor value
-        d_left = []
-        d_tangent = 999
-        d_right = []
-
         count = (int)(scan.scan_time / scan.time_increment)
-        self.MSG.data = False
+        #self.MSG.data = False
         wall_position_array = geometry_msgs.msg.PoseArray()
         for i in range(0, count): 
             x = scan.angle_min + scan.angle_increment * i
             degree = ((x)*180./3.14)
             # print(str(i) + ' ' + str(degree) + ' ' + str(scan.ranges[i]))
-
-            # publish the distance sensor                         
-            if i >= 100 and i <= 160: # right +80deg
-                d_right.append(scan.ranges[i])
-            elif i == 180: # straight ahead
-                d_tangent = [scan.ranges[i]]
-            elif i < 260 and i >= 200: # left -80deg
-                d_left.append(scan.ranges[i])
-            elif i == 260:
-                #print(round(d_left,2), round(d_tangent,2), round(d_right,2))   
-                print [round(a,2) for a in d_left]
-
-                distance_sensor = Float32MultiArray()
-                distance_sensor.data = [d_left + d_tangent + d_right]
-                self.pub_WALL_DISTANCE.publish(distance_sensor)
-
-        '''
         
-            if (i >= 160) and (i < 200):# -20 to 20
+            if (i >= 160) and (i < 200) and not self.MSG.data:# -20 to 20
                 if scan.ranges[i] < self.DISTANCE_THRESHOLD:
                     self.MSG.data = True
+                    self.pub_WALL_DETECTION.publish(self.MSG)
+                    self.PUB_POS = True
                 else:
                     pass
             else:
                 pass
             
-            if scan.ranges[i] != float("inf"):
+            if scan.ranges[i] != float("inf") and (i >= 160) and (i < 200) and self.MSG.data:
 
                 #print(str(i) + ' ' + str(degree) + ' ' + str(scan.ranges[i]))
                 #self.WALL_POSITION.header.stamp = rospy.Time.now()
@@ -112,7 +90,9 @@ class Wall_Detection:
                 #print(str(self.WALL_POSITION.point.x))
 
                 # THIS IS TEMPORARILY DISABLED#########################################################
-                #self.pub_WALL_POSITION.publish(wall_position_array)         
+        if self.PUB_POS:
+            self.pub_WALL_POSITION.publish(wall_position_array)   
+            self.PUB_POS = False      
         
         
     #####################################################
@@ -133,7 +113,7 @@ class Wall_Detection:
     #####################################################
     def loop(self):
         while not rospy.is_shutdown():
-            self.pub_WALL_DETECTION.publish(self.MSG)
+            
 
 
             self.rate.sleep()
